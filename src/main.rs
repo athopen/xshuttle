7fe1b99efda236e137f27e84129b23cd96188963
Andreas Penz <andreas.penz.1989@gmail.com>
2026-01-06 14:38:26 +0100

    separate platform code from `Application` struct

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,93 @@
 mod xshuttle;
 
 use clap::Parser;
-use winit::event_loop::EventLoop;
-use xshuttle::{Application, UserEvent};
-
-#[cfg(not(target_os = "linux"))]
 use tray::MenuEvent;
+use winit::application::ApplicationHandler;
+use winit::event::StartCause;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use xshuttle::{Application, UserEvent};
 
 const VERSION: &str = concat!(env!("XSHUTTLE_VERSION"), " ", env!("XSHUTTLE_BUILD_HASH"));
 
 #[derive(Parser)]
 #[command(name = "xshuttle", version = VERSION)]
 struct Arguments {}
+
+impl ApplicationHandler<UserEvent> for Application {
+    fn new_events(&mut self, _event_loop: &ActiveEventLoop, cause: StartCause) {
+        if cause == StartCause::Init {
+            #[cfg(not(target_os = "linux"))]
+            self.init();
+
+            #[cfg(target_os = "macos")]
+            wake_macos_run_loop();
+        }
+    }
+
+    fn resumed(&mut self, _event_loop: &ActiveEventLoop) {}
+
+    fn user_event(&mut self, event_loop: &ActiveEventLoop, event: UserEvent) {
+        let UserEvent::MenuEvent(event) = event;
+
+        // On Linux, this is a quit signal from the GTK thread
+        #[cfg(target_os = "linux")]
+        {
+            let _ = event;
+            event_loop.exit();
+        }
+
+        // On other platforms, handle the menu event directly
+        #[cfg(not(target_os = "linux"))]
+        if self.handle_menu_event(event) {
+            event_loop.exit();
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        _event_loop: &ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        _event: winit::event::WindowEvent,
+    ) {
+    }
+}
+
+#[cfg(target_os = "macos")]
+fn wake_macos_run_loop() {
+    use objc2_core_foundation::CFRunLoop;
+    if let Some(rl) = CFRunLoop::main() {
+        rl.wake_up();
+    }
+}
+
+#[cfg(target_os = "linux")]
+fn run_gtk_thread(quit_proxy: winit::event_loop::EventLoopProxy<UserEvent>) {
+    use tray::MenuId;
+
+    std::thread::spawn(move || {
+        gtk::init().unwrap();
+
+        let mut app = Application::default();
+        app.init();
+
+        let receiver = MenuEvent::receiver();
+        gtk::glib::timeout_add_local(std::time::Duration::from_millis(50), move || {
+            while let Ok(event) = receiver.try_recv() {
+                if app.handle_menu_event(event) {
+                    let quit_event = MenuEvent {
+                        id: MenuId::new("quit"),
+                    };
+                    let _ = quit_proxy.send_event(UserEvent::MenuEvent(quit_event));
+                    gtk::main_quit();
+                    return gtk::glib::ControlFlow::Break;
+                }
+            }
+            gtk::glib::ControlFlow::Continue
+        });
+
+        gtk::main();
+    });
+}
 
 fn main() {
     Arguments::parse();
@@ -20,7 +96,7 @@ fn main() {
     let proxy = event_loop.create_proxy();
 
     #[cfg(target_os = "linux")]
-    xshuttle::run_gtk_thread(proxy);
+    run_gtk_thread(proxy);
 
     #[cfg(not(target_os = "linux"))]
     MenuEvent::set_event_handler(Some(move |event| {
--- src/xshuttle.rs
+++ src/xshuttle.rs
@@ -4,9 +4,6 @@ use config::{Config, config_path, ensure_config_exists, load};
 use ssh::parse_ssh_config;
 use terminal::Terminal;
 use tray::{MENU_ID_CONFIGURE, MENU_ID_QUIT, MENU_ID_RELOAD, Menu, MenuEvent, Tray, build_menu};
-use winit::application::ApplicationHandler;
-use winit::event::StartCause;
-use winit::event_loop::ActiveEventLoop;
 
 #[derive(Debug)]
 pub enum UserEvent {
@@ -120,84 +117,4 @@ fn is_terminal_editor(editor: &str) -> bool {
         editor,
         "nano" | "vim" | "vi" | "nvim" | "emacs" | "micro" | "ne" | "joe" | "pico" | "ed"
     )
-}
-
-impl ApplicationHandler<UserEvent> for Application {
-    fn new_events(&mut self, _event_loop: &ActiveEventLoop, cause: StartCause) {
-        if cause == StartCause::Init {
-            // On Linux, the app is initialized in the GTK thread instead
-            #[cfg(not(target_os = "linux"))]
-            self.init();
-
-            #[cfg(target_os = "macos")]
-            wake_macos_run_loop();
-        }
-    }
-
-    fn resumed(&mut self, _event_loop: &ActiveEventLoop) {}
-
-    fn user_event(&mut self, event_loop: &ActiveEventLoop, event: UserEvent) {
-        let UserEvent::MenuEvent(event) = event;
-
-        // On Linux, this is a quit signal from the GTK thread
-        #[cfg(target_os = "linux")]
-        {
-            let _ = event;
-            event_loop.exit();
-        }
-
-        // On other platforms, handle the menu event directly
-        #[cfg(not(target_os = "linux"))]
-        if self.handle_menu_event(event) {
-            event_loop.exit();
-        }
-    }
-
-    fn window_event(
-        &mut self,
-        _event_loop: &ActiveEventLoop,
-        _window_id: winit::window::WindowId,
-        _event: winit::event::WindowEvent,
-    ) {
-    }
-}
-
-#[cfg(target_os = "macos")]
-fn wake_macos_run_loop() {
-    // Wake the run loop to ensure the tray icon appears immediately
-    use objc2_core_foundation::CFRunLoop;
-    if let Some(rl) = CFRunLoop::main() {
-        rl.wake_up();
-    }
-}
-
-#[cfg(target_os = "linux")]
-pub fn run_gtk_thread(quit_proxy: winit::event_loop::EventLoopProxy<UserEvent>) {
-    // On Linux, winit doesn't use GTK but tray-icon requires it.
-    // Run the app in a dedicated GTK thread.
-    std::thread::spawn(move || {
-        gtk::init().unwrap();
-
-        let mut app = Application::default();
-        app.init();
-
-        // Poll for menu events in the GTK main loop
-        let receiver = MenuEvent::receiver();
-        gtk::glib::timeout_add_local(std::time::Duration::from_millis(50), move || {
-            while let Ok(event) = receiver.try_recv() {
-                if app.handle_menu_event(event) {
-                    // Signal the winit event loop to exit
-                    let quit_event = MenuEvent {
-                        id: tray::MenuId::new("quit"),
-                    };
-                    let _ = quit_proxy.send_event(UserEvent::MenuEvent(quit_event));
-                    gtk::main_quit();
-                    return gtk::glib::ControlFlow::Break;
-                }
-            }
-            gtk::glib::ControlFlow::Continue
-        });
-
-        gtk::main();
-    });
 }
